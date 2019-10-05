2ee8347dd9bd790bf7bf98badd56ace5a86fdce9
Federico <45139706+fededev01@users.noreply.github.com>
2019-10-05 22:49:41 +0200

    Create writeln.java

---
+++ writeln.java
@@ -0,0 +1,8 @@
+public class Solar {
+  public static void main(String[] args) {
+   System.out.printlln("Hello World");
+   System.out.print("Hello World 2"); 
+   System.out.printlln("Hello World 3"); 
+   System.out.printlln("Hello World 4\n"); 
+   }
+}   
