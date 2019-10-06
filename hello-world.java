dad13686d9a3f76a3faf163d7af0a21b222c8832
Federico <45139706+fededev01@users.noreply.github.com>
2019-10-06 21:41:59 +0200

    Update hello-world.java

--- hello-world.java
+++ hello-world.java
@@ -1,12 +1,20 @@
+//every program in Java must have a class;
+//in Java each code statement must end with a semicolon;
+//don't use semicolons after method and class declaration;
 public class Solar {
+//Solar is the name of the class;
 //A single-line comment starts with two forward slashes;
 //Note that every single-line comment must and with a semicolon;
   public static void main(String[] args) {
+//void indicate that a method doesn't return a value;
 //main is the name of the method;
 //method's parameters are declared inside the parentheses that follow the name of the method;
-   System.out.printlln("Hello World");
+   System.out.println("Hello World");
+//this is the body of the main method, enclosed in curly braces;
+//println method prints a line of text;
+//the System class and it's out stream are used to access the println method;
    System.out.print("Hello World 2"); 
-   System.out.printlln("Hello World 3"); 
-   System.out.printlln("Hello World 4\n"); 
+   System.out.println("Hello World 3"); 
+   System.out.println("Hello World 4\n"); 
    }
 }   
