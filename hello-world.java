2ce88d5752f86ab764f52be353d2cc5c32292a97
Federico <45139706+fededev01@users.noreply.github.com>
2019-10-06 07:26:47 +0200

    Update writeln.java
    Add four comments and modify the file name

---
+++ hello-world.java
@@ -0,0 +1,12 @@
+public class Solar {
+//A single-line comment starts with two forward slashes;
+//Note that every single-line comment must and with a semicolon;
+  public static void main(String[] args) {
+//main is the name of the method;
+//method's parameters are declared inside the parentheses that follow the name of the method;
+   System.out.printlln("Hello World");
+   System.out.print("Hello World 2"); 
+   System.out.printlln("Hello World 3"); 
+   System.out.printlln("Hello World 4\n"); 
+   }
+}   
--- writeln.java
+++
@@ -1,8 +0,0 @@
-public class Solar {
-  public static void main(String[] args) {
-   System.out.printlln("Hello World");
-   System.out.print("Hello World 2"); 
-   System.out.printlln("Hello World 3"); 
-   System.out.printlln("Hello World 4\n"); 
-   }
-}   
