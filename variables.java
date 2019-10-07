124f4b2cbd23b3aa4ff3323f33b88c954d4cc912
Meziu <55318903+Meziu@users.noreply.github.com>
2019-10-07 21:19:05 +0200

    Aggiunte spiegazioni dettagliate alle variabili
    Ho scritto delle spiegazioni dettagliate riguardo l'inizializzazione delle variabili e i vari tipi di valori che possono contenere, se ci sono degli errori grammaticali in inglese fatemi sapere

--- variables.java
+++ variables.java
@@ -1,3 +1,19 @@
 //variables store data for processing;
 //a variable is given a name (or identifier) such as area or age;
 //the name uniquely identifies each variable, assigning a value to the variable and retrieving the value stored;  
+int number = 3; //this is an example of a variable
+//as you can see above, there are 4 indicators to inizializate a variable
+//the first(int in this example) indicates the value type
+//the second is the variable name, in this case number
+//the third is the = (equal) simbol, which inizializate the variable to a value
+//the fourth is the value itself (in this case 3) that is assigned to the variable
+/*the semicolon at the end of the line is just a piece of the java syntax, it means the line of code finishes,
+you're obligated to add it or else the console will give a syntax error*/
+
+//The value types are:
+//int (stands for integer), this value is a number without decimals
+//double, this value can be both values with or without decimals, but will automatically add a .0 if there is an integer number in it 
+//char (stands for character), this value is a single text caracter, its value can be setted with the ''
+//string, this value is made out of multiple chars which forms a line of text, its value can be setted with the ""
+//bool (stands for boolean), this value can be only true or false, its value can be setted by typing only true or false, without the ""
+//all this value types must be written in the variable inizialization ahead of the varable's name
