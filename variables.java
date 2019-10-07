8986625db499c47f2b46d880969bc9810715ba78
Federico <45139706+fededev01@users.noreply.github.com>
2019-10-07 08:31:12 +0200

    Update variables.java

--- variables.java
+++ variables.java
@@ -1,1 +1,3 @@
-
+//variables store data for processing;
+//a variable is given a name (or identifier) such as area or age;
+//the name uniquely identifies each variable, assigning a value to the variable and retrieving the value stored;  
