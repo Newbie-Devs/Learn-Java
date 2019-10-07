8cec759d958a6cbdc97ee85fecd9d5afd780ebc6
Federico <45139706+fededev01@users.noreply.github.com>
2019-10-08 00:17:28 +0200

    Update variables.java

--- variables.java
+++ variables.java
@@ -1,7 +1,8 @@
 //variables store data for processing;
 //a variable is given a name (or identifier) such as area or age;
 //the name uniquely identifies each variable, assigning a value to the variable and retrieving the value stored;  
-int number = 3; //this is an example of a variable
+int number = 3; 
+//this is an example of a variable
 //as you can see above, there are 4 indicators to inizializate a variable
 //the first(int in this example) indicates the value type
 //the second is the variable name, in this case number
@@ -15,5 +16,22 @@ you're obligated to add it or else the console will give a syntax error*/
 //double, this value can be both values with or without decimals, but will automatically add a .0 if there is an integer number in it 
 //char (stands for character), this value is a single text caracter, its value can be setted with the ''
 //string, this value is made out of multiple chars which forms a line of text, its value can be setted with the ""
+String name = "David";
+//this creates a variable called name of type string and assigns it the value "David"
 //bool (stands for boolean), this value can be only true or false, its value can be setted by typing only true or false, without the ""
 //all this value types must be written in the variable inizialization ahead of the varable's name
+
+//examples of variable declarations:
+class MyClass {
+  public static void main(String[] args) {
+    String name ="David";
+    int age = 18;
+    double score = 15.9;
+    char group = "Z";
+    boolean online = true;
+    }
+  }
+/* you can use a comma-separated list to declare
+  more than one variable of the specified type */
+int a = 42, b = 23;
+  
