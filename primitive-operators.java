b5fc62f50e8dcbb2de57bc4a0ae39eb52623acf7
Federico <45139706+fededev01@users.noreply.github.com>
2019-10-09 18:55:57 +0200

    Update primitive-operators.java

--- primitive-operators.java
+++ primitive-operators.java
@@ -12,3 +12,19 @@ int x = 6 + 3;
 way  that they are used in algebric equations */
 /* The + operator adds together two values, such as two constants,
 a constant and a variable, or two variables */
+int sum1 = 50 + 10;
+//In this example sum1 will store the result of the sum
+int sum2 = sum1 + 66;
+//In this case we add 66 to the variable sum1
+int sun2 = sum1 + sum2;
+//In this example we add two wariables
+//The - operator subtracts one value to an another
+int sut1 = 1000 - 10;
+//In this example, sut1 will store the result of the subtraction
+int sut2 = sut1 - 50;
+//In this case, we subtract 50 to the variable sut1
+int sut3 = sut1 - sut2;
+//In this example, we subtract the variable sut2 to the variable sut1
+/*Just like in algebra, you can use both of the operations in
+a single line */
+int pro = 10 + 5 - 2;
