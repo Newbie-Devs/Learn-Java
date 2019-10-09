f115e83119869511f715a913a773269a50145e2f
Federico <45139706+fededev01@users.noreply.github.com>
2019-10-09 18:13:28 +0200

    Update primitive-operators.java

--- primitive-operators.java
+++ primitive-operators.java
@@ -1,1 +1,14 @@
-
+//Java provides a rich set of operators to use in manipulating variables
+//A value used on either side of an operator is called an operand
+int x = 6 + 3;
+//In this expression, 6 and 3 are operands of the plus operator
+/* Java arithmetic operators are:
++ addition
+- subtraction
+* multiplication
+/ division
+% modulo */
+/* Arithmetic operators are used in mathematical expressions in the same
+way  that they are used in algebric equations */
+/* The + operator adds together two values, such as two constants,
+a constant and a variable, or two variables */
