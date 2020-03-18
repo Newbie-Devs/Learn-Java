d4bc0e496a206d2f4125a07622cc5a4cd79a08dd
Federico <45139706+fededev01@users.noreply.github.com>
2020-03-18 18:19:56 +0100

    Update primitive-operators.java

--- primitive-operators.java
+++ primitive-operators.java
@@ -10,14 +10,16 @@ int x = 6 + 3;
 % modulo */
 /* Arithmetic operators are used in mathematical expressions in the same
 way  that they are used in algebric equations */
+
 /* The + operator adds together two values, such as two constants,
 a constant and a variable, or two variables */
 int sum1 = 50 + 10;
 //In this example sum1 will store the result of the sum
 int sum2 = sum1 + 66;
 //In this case we add 66 to the variable sum1
-int sun2 = sum1 + sum2;
+int sum3 = sum1 + sum2;
 //In this example we add two wariables
+
 //The - operator subtracts one value to an another
 int sut1 = 1000 - 10;
 //In this example, sut1 will store the result of the subtraction
@@ -28,4 +30,13 @@ int sut3 = sut1 - sut2;
 /*Just like in algebra, you can use both of the operations in
 a single line */
 int pro = 10 + 5 - 2;
+
 //The * operator multiplies two values.
+int mult1 = 1000 * 2;
+//In this example mult1 will store the result of the multiplication
+int mult2 = mult1 * 10;
+//In this case we multiplicate 10 for the variable mult1
+int mult3 = mult1 + mult2;
+//In this example we multiplicate two wariables
+
+//The / operator divides one value by another
