584144e24c19a0b8f7ef2fe7548301be2507b13d
Federico <45139706+fededev01@users.noreply.github.com>
2020-03-18 15:03:05 +0100

    Update variables.java
    Update string description

--- variables.java
+++ variables.java
@@ -6,11 +6,10 @@
 //int (integer, whole numbers ), this value is a number without decimals
 //double, for floating-point or real numbers with optional decimal points and fractional parts in fixed or scientific notations, such as 3.12 or -55.66
 //char (stands for character), holds only a single character. 
-//string, this value is made out of multiple chars which forms a line of text. 
+//String, for texts such as "Hello" or "Good Morning". Text strings are enclosed within double quotes. 
 String name = "David";
 //this creates a variable called name of type string and assigns it the value "David"
 //bool (stands for boolean), this value can be only true or false. This data type is used for simple flags that track true/false conditions. 
-//all this value types must be written in the variable initialization ahead of the varable's name
 
 //examples of variable declarations:
 class Solar {
