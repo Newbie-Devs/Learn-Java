e407a498cbc30748b9330a5cacdc52b6d5c24612
Federico <45139706+fededev01@users.noreply.github.com>
2020-04-11 12:22:56 +0200

    Add files via upload

---
+++ docs/variables.java
@@ -0,0 +1,27 @@
+//variables store data for processing;
+//a variable is given a name (or identifier) such as area or age;
+//the name uniquely identifies each variable, assigning a value to the variable and retrieving the value stored;  
+
+//Variables have types:
+//int (integer, whole numbers ), this value is a number without decimals
+//double, for floating-point or real numbers with optional decimal points and fractional parts in fixed or scientific notations, such as 3.12 or -55.66
+//char (stands for character), holds only a single character. 
+//String, for texts such as "Hello" or "Good Morning". Text strings are enclosed within double quotes. 
+String name = "David";
+//this creates a variable called name of type string and assigns it the value "David"
+//bool (stands for boolean), this value can be only true or false. This data type is used for simple flags that track true/false conditions. 
+
+//examples of variable declarations:
+class Solar {
+  public static void main(String[] args) {
+    String name ="David";
+    int age = 18;
+    double score = 15.9;
+    char group = "Z";
+    boolean online = true;
+    }
+  }
+/* you can use a comma-separated list to declare
+  more than one variable of the specified type */
+int a = 42, b = 23;
+  
